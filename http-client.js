dc696519c5d81e47e5b782666d90bb2db2f9173d
yuanyu90221 <yuanyu90221@gmail.com>
2020-10-08 10:53:28 +0800

    ad http-client

---
+++ http-client.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const url = process.argv[2];
+http.get(url, function(res){ 
+  res.setEncoding('utf-8');  
+  res.on('data', console.log);
+ res.on("error", console.error);
+}).on('error', console.error);
